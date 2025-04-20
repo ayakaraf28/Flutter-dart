e0c47542f1bb7236e5310ed5b2712ecb41bbcf7c
ayakaraf28 <ayakaraf2002@gmail.com>
2025-04-21 00:27:55 +0300

    update

---
+++ aya_project.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name="aya";
+  print(name[0].toUpperCase());
+  int age= 22; 
+  double myhighest=-156.5;
+  print(age);
+  print(myhighest.floor());
+  print(myhighest.round());
+  print(myhighest.isNegative);
+  print(myhighest.abs());
+}
